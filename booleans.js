d5fdec55aa4f7b41946e1ef694a9c60dd8bf9c59
Fubarahart <fubarahart@gmail.com>
2026-09-25 07:23:52 +0100

    New

--- booleans.js
+++ booleans.js
@@ -113,3 +113,22 @@ function golfScore(par, strokes) {
 }
 console.log(golfScore(5, 4));
 console.log(golfScore(4, 3));
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+    }
---
+++ objectValues.js
@@ -0,0 +1,3 @@
+let value3 = "true String";
+let value4 = new Boolean(value3);
+console.log(value4)
